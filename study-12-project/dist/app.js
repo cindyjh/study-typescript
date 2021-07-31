c1ef26fb57b49cfdd5af8a95c1b131fb89b2ff9c
joohee <xorjsqnfl@gmail.com>
2021-07-31 19:44:37 +0900

    기본 component들 만들기(video, todo, note, image)

--- study-12-project/dist/app.js
+++ study-12-project/dist/app.js
@@ -1,11 +1,20 @@
-import { ImageComponent } from "./components/page/item/image.js";
-import { PageComponent } from "./components/page/page.js";
+import { ImageComponent } from "./components/items/image.js";
+import { NoteComponent } from "./components/items/note.js";
+import { PageComponent } from "./components/items/page.js";
+import { TodoComponent } from "./components/items/todo.js";
+import { VideoComponent } from "./components/items/video.js";
 class App {
     constructor(appRoot) {
         this.page = new PageComponent();
         this.page.attachTo(appRoot);
-        this.image = new ImageComponent("첫번째이미지", "https://picsum.photos/600/300");
+        this.image = new ImageComponent("ImageComponent Title", "https://picsum.photos/600/300");
         this.image.attachTo(appRoot, 'beforeend');
+        const video = new VideoComponent("videoComponent Title", "https://www.youtube.com/embed/iOt5AZmGg5o");
+        video.attachTo(appRoot, 'beforeend');
+        const note = new NoteComponent('NoteComponent Title', 'body');
+        note.attachTo(appRoot, "beforeend");
+        const todo = new TodoComponent("TodoComponent Title", 'todo');
+        todo.attachTo(appRoot, "beforeend");
     }
 }
 new App(document.querySelector('.document'));
--- study-12-project/src/app.ts
+++ study-12-project/src/app.ts
@@ -1,5 +1,8 @@
-import { ImageComponent } from "./components/page/item/image.js";
-import { PageComponent } from "./components/page/page.js";
+import { ImageComponent } from "./components/items/image.js";
+import { NoteComponent } from "./components/items/note.js";
+import { PageComponent } from "./components/items/page.js";
+import { TodoComponent } from "./components/items/todo.js";
+import { VideoComponent } from "./components/items/video.js";
 
 class App {
     private readonly page: PageComponent;
@@ -9,8 +12,18 @@ class App {
         this.page = new PageComponent();
         this.page.attachTo(appRoot);
 
-        this.image = new ImageComponent("첫번째이미지", "https://picsum.photos/600/300");
+        this.image = new ImageComponent("ImageComponent Title", "https://picsum.photos/600/300");
         this.image.attachTo(appRoot, 'beforeend');
+
+        const video = new VideoComponent("videoComponent Title", "https://www.youtube.com/embed/iOt5AZmGg5o")
+        video.attachTo(appRoot, 'beforeend');
+
+        const note = new NoteComponent('NoteComponent Title', 'body');
+        note.attachTo(appRoot, "beforeend");
+
+        const todo = new TodoComponent("TodoComponent Title", 'todo')
+        todo.attachTo(appRoot, "beforeend")
+
     } 
 }
 
